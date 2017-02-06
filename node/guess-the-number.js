d643392727463d28109caf5aa8e64ffe31a59989
Amelia Clarke <ameliavoncat@gmail.com>
2017-02-06 12:06:14 -0800

    Guess The Number (js)

---
+++ node/guess-the-number.js
@@ -0,0 +1,30 @@
+const colors = require('colors')
+const quitCommand = require('./helpers/quitCommand')
+let computerNumber;
+
+function generateNumber(){
+  computerNumber = Math.ceil(Math.random() * 100)
+}
+
+generateNumber();
+console.log("Guess the number I'm thinking of!".green)
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function(input){
+  if(input.indexOf('/') === 0) {
+    quitCommand(input, process)
+  } else {
+    input = Number(input.replace(/\D/, ''))
+
+    if(input === computerNumber){
+      console.log('Congratulations! You guessed the right number!'.green)
+      process.disconnect();
+    } else if (input === NaN){
+      console.log('Guesses must be a number!'.green)
+    } else {
+      console.log('Guess again!'.green)
+    }
+  }
+})
