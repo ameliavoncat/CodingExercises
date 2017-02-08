a7f3cb112a7a10344d4788ec9baf738b9c06b1ec
Amelia Clarke <ameliavoncat@gmail.com>
2017-02-07 18:28:20 -0800

    Lairotcaf (rb)

--- ruby/constants/menuOptions.rb
+++ ruby/constants/menuOptions.rb
@@ -6,5 +6,6 @@
   :'5' => {path: './ruby/exercises/the-disemvowling.rb', name: 'The Disemvowling'},
   :'6' => {path: './ruby/exercises/where-json.rb', name: 'Where In The JSON?'},
   :'7' => {path: './ruby/exercises/who-wants-a-date.rb', name: 'Who Wants A Date?'},
-  :'8' => {path: './ruby/exercises/parens-be-gone.rb', name: 'Parens Be Gone'}
+  :'8' => {path: './ruby/exercises/parens-be-gone.rb', name: 'Parens Be Gone'},
+  :'9' => {path: './ruby/exercises/lairotcaf.rb', name: 'Lairotcaf'}
 }
