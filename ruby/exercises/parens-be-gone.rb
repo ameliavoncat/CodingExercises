30691f3671e4f12c28729f815ffc541783844b6c
Amelia Clarke <ameliavoncat@gmail.com>
2017-02-10 16:21:40 -0800

    Parens Be Gone (rb) fixup

--- ruby/exercises/parens-be-gone.rb
+++ ruby/exercises/parens-be-gone.rb
@@ -6,15 +6,41 @@ def displayExitPrompt
   exitPrompt
 end
 
+def removeExtraParens(string)
+  openParens = []
+  previousMatch = {
+    :open => '',
+    :close => ''
+  }
+
+  string = string.split('')
+  string.each_index {|index|
+    character = string[index]
+    if(character === '(')
+      openParens.push(index)
+    elsif(character === ')')
+      openBracket = openParens.pop()
+      if(openBracket === previousMatch[:open].to_i - 1 && previousMatch[:close].to_i === index - 1)
+        string[openBracket] = ''
+        string[index] = ''
+      elsif(openBracket + 1 === index)
+        string[openBracket] = ''
+        string[index] = ''
+      end
+      previousMatch[:open] = openBracket
+      previousMatch[:close] = index
+    end
+  }
+
+  return string.join('')
+end
+
 
 puts('Enter an expression to correct.'.green)
-input = gets.sub(/\n/, '')
+input = gets.chomp
 quitToMenu(input)
+result = removeExtraParens(input)
 
-puts(input)
-input.gsub!(/\({2,}/, '(').gsub!(/\){2,}/, ')')
-
-puts('Parens corrected!'.green)
-puts(input.green)
+puts('The corrected string is: '.green + result.yellow)
 
 displayExitPrompt
