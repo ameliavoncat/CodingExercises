9c222f0a67167401e1088491dca645afd37741c2
Amelia Clarke <ameliavoncat@gmail.com>
2017-03-15 17:21:16 -0700

    initial setup and menu prompt

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# CodingExercises
+## Coding Exercises
+
+### To Run
+- Enter `bundle install` to install the gem dependencies.
+- Enter `ruby start` to see the menu.
+
+### Completed
+#### Ruby
+- Braille
+- Fibonnaci Bases
+- Finite State Gate
+- Guess The Number
+- Lairotcaf
+- Scrab Bag
+- String Indices
+- String Permutations
+- The Disemvowling
+- Where In The JSON?
+- Who Wants A Date?
+- Parens Be Gone
+
+### Original Project Description
+https://github.com/GuildCrafts/web-development-js/issues/144
+
+<!-- LICENSE -->
+Licensed under the [MIT License][mit-license].
+
+[mit-license]: https://github.com/ameliavoncat/CodingExercises/blob/master/LICENSE
---
+++ ruby/constants/menu_options.rb
@@ -0,0 +1,14 @@
+$menu_options = {
+  :'1' => {path: './ruby/exercises/guess_the_number.rb', name: 'Guess The Number'},
+  :'2' => {path: './ruby/exercises/scrab_bag.rb', name: 'Scrab Bag'},
+  :'3' => {path: './ruby/exercises/string_indices.rb', name: 'String Indices'},
+  :'4' => {path: './ruby/exercises/fibonacci_bases.rb', name: 'Fibonnacci Bases'},
+  :'5' => {path: './ruby/exercises/the_disemvowling.rb', name: 'The Disemvowling'},
+  :'6' => {path: './ruby/exercises/where_in_the_json.rb', name: 'Where In The JSON?'},
+  :'7' => {path: './ruby/exercises/who_wants_a_date.rb', name: 'Who Wants A Date?'},
+  :'8' => {path: './ruby/exercises/parens_be_gone.rb', name: 'Parens Be Gone'},
+  :'9' => {path: './ruby/exercises/lairotcaf.rb', name: 'Lairotcaf'},
+  :'10' => {path: './ruby/exercises/finite_state_gate.rb', name: 'Finite State Gate'},
+  :'11' => {path: './ruby/exercises/string_permutations.rb', name: 'String Permutations'},
+  :'12' => {path: './ruby/exercises/braille.rb', name: 'Braille'}
+}
